b7bb5f213c4892aeccd8d87fcdc8012e61295d6b
soshiharami <soshi0807sss@gmail.com>
2023-03-07 22:40:31 +0900

    fix: setup favicon

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -88,25 +88,29 @@ const favicons: Array<React.ComponentPropsWithoutRef<'link'>> = [
   {
     rel: 'apple-touch-icon',
     sizes: '180x180',
-    href: '/favicon/apple-touch-icon.png',
+    href: '/favicon-32x32.png',
   },
   {
     rel: 'icon',
     type: 'image/png',
     sizes: '32x32',
-    href: '/favicon/favicon-32x32.png',
+    href: '/favicon-32x32.png',
   },
   {
     rel: 'icon',
     type: 'image/png',
     sizes: '16x16',
-    href: '/favicon/favicon-16x16.png',
+    href: '/favicon-32x32.png',
   },
   { rel: 'manifest', href: '/favicon/site.webmanifest' },
   {
     rel: 'mask-icon',
-    href: '/favicon/safari-pinned-tab.svg',
+    href: '/favicon-32x32.png',
     color: '#00e887',
   },
-  { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+  {
+    rel: 'shortcut icon',
+
+    href: '/favicon-32x32.png',
+  },
 ];
